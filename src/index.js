5474f59725c9e169f4c1e26a97e8c13c31f77662
Gaagul Gigi <70290286+gaagul@users.noreply.github.com>
2023-09-19 00:06:52 +0530

    1.0.1 (#2)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-/**
- * The entrypoint for the action.
- */
-const { run } = require('./main')
+const core = require('@actions/core')
+
+// const bumpGem = require("./bump");
+const bumpFrontend = require('./bumpFrontend')
+
+const run = async () => {
+  try {
+    const labels = core.getInput('labels').split(',')
+    // if(labels.includes("backend")) {
+    //   await bumpGem();
+    // }
+
+    if (labels.includes('frontend')) {
+      core.info('Bumping frontend version...')
+      await bumpFrontend(labels)
+    }
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
 
 run()
